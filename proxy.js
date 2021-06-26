ac997f498400587f8c8a99dc12faccd8aadaf9e2
Alex Smith <alegorn.mail@gmail.com>
2021-06-26 23:16:31 +0700

    Start

---
+++ proxy.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const url = require('url');
+const request = require('request');
+
+const hostname = '127.0.0.1';
+const port = 3003;
+
+const server = http.createServer((req, res) => {
+    let myResponse = {};
+
+    const queryObject = url.parse(req.url,true).query;
+
+    let requestUrl = queryObject.url;
+
+    if (requestUrl){
+        console.log("OK");
+
+        request(requestUrl, { json: true }, (perr, pres, pbody) => {
+            if (perr) { return console.log(perr); }
+            
+             Object.assign(myResponse,pbody) ;
+
+             res.statusCode = 200;
+             res.setHeader('Content-Type', 'application/json');
+             res.end(JSON.stringify(myResponse));
+        });        
+    }
+    else {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Parameter "url" not found');
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Proxy running at http://${hostname}:${port}/`);
+});
